ebc3c75d70ee9bb8fe2b2b05190c6a61dbf8a46c
Aaron Buxbaum <me@aaronbuxbaum.com>
2019-02-25 22:32:18 -0500

    4.11

--- 4. Trees and Graphs/4.11 - Random Node.js
+++ 4. Trees and Graphs/4.11 - Random Node.js
@@ -5,3 +5,76 @@
   Design and implement an algorithm for getRandomNode, and explain how you would
   implement the rest of the methods.
  */
+
+import { random } from "lodash";
+
+class RandomBinaryTreeNode {
+  size = 1;
+
+  constructor(value) {
+    this.value = value;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = new RandomBinaryTreeNode(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new RandomBinaryTreeNode(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+
+    this.size++;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  getNthNode(n) {
+    const leftSize = this.left ? this.left.getSize() : 0;
+
+    if (n < leftSize) {
+      return this.left.getNthNode(n);
+    } else if (n === leftSize) {
+      return this;
+    } else {
+      const rightSize = n - (leftSize + 1);
+      return this.right.getNthNode(rightSize);
+    }
+  }
+}
+
+export class RandomBinaryTree {
+  insert(value) {
+    if (!this.root) {
+      this.root = new RandomBinaryTreeNode(value);
+    } else {
+      this.root.insert(value);
+    }
+  }
+
+  getSize() {
+    return this.root ? this.root.getSize() : 0;
+  }
+
+  // ignored for now
+  find(node) {}
+
+  // ignored for now
+  delete(node) {}
+
+  getRandomNode() {
+    const seed = random(0, this.getSize());
+    return this.root.getNthNode(seed);
+  }
+}
+
+// Time complexity: O(log N) time to insert, O(log N) to find random node
+// Space complexity: O(N) for the nodes themselves, O(log N) to find random node for recursive stacks
--- 4. Trees and Graphs/__tests__/4.11 - Random Node.js
+++ 4. Trees and Graphs/__tests__/4.11 - Random Node.js
@@ -1,1 +1,18 @@
-test.todo("Random Node");
+import { RandomBinaryTree } from "../4.11 - Random Node";
+
+jest.unmock("lodash");
+const lodash = require.requireActual("lodash");
+
+test("Random Node", () => {
+  lodash.random = () => 4;
+
+  const tree = new RandomBinaryTree();
+
+  tree.insert(1);
+  tree.insert(5);
+  tree.insert(3);
+  tree.insert(8);
+  tree.insert(2);
+
+  expect(tree.getRandomNode().value).toEqual(8);
+});
